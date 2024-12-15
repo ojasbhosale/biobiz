22684c38bcb0d45a1fb9a5e43671bf968b1fe961
ojasbhosale <ojasbhosale07@gmail.com>
2024-12-15 17:25:03 +0530

    1 commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Smooth Scrolling for Navbar Links
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
